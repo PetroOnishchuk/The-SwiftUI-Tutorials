f8a0e944aa20ce19db3847aa3739f67bd42b0691
PetroOnishchuk <petroonishchuk@icloud.com>
2021-01-03 16:44:06 +0200

    Second commit

---
+++ VStackProject01/Shared/VStackProject01App.swift
@@ -0,0 +1,17 @@
+//
+//  VStackProject01App.swift
+//  Shared
+//
+//  Created by Petro Onishchuk on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct VStackProject01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
