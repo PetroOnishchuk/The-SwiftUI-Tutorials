d16d9fd588a0928795c45baa71e43e3e83c2aded
PetroOnishchuk <petroonishchuk@icloud.com>
2021-02-25 18:37:33 +0200

    Create Project NavigationViewNavigationLinkAndList

---
+++ NavigationViewNavigationLinkAndList/Shared/FirstEmojiView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Petro Onishchuk on 2/25/21.
+//
+
+import SwiftUI
+
+struct FirstEmojiView: View {
+    
+    var body: some View {
+        
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstEmojiView()
+    }
+}
---
+++ NavigationViewNavigationLinkAndList/Shared/NavigationViewNavigationLinkAndListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewNavigationLinkAndListApp.swift
+//  Shared
+//
+//  Created by Petro Onishchuk on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewNavigationLinkAndListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FirstEmojiView()
+        }
+    }
+}
