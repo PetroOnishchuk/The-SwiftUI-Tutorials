0c157ff1cee3fd7eda1f9e9264c0e20063427282
PetroOnishchuk <petroonishchuk@icloud.com>
2022-02-20 19:02:03 +0200

    Project CoreData&SwiftUICustomPart01 Completed

---
+++ CoreData&SwiftUI01/CoreData&SwiftUIPart01/CoreData&SwiftUICustomPart01/CoreData&SwiftUICustomPart01/CoreData/Persistence.swift
@@ -0,0 +1,28 @@
+//
+//  Persistence.swift
+//  CoreData&SwiftUICustomPart01
+//
+//  Created by Petro Onishchuk on 2/20/22.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "BooksML")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
--- CoreData&SwiftUI01/CoreData&SwiftUIPart01/CoreData&SwiftUICustomPart01/CoreData&SwiftUICustomPart01/CoreData_SwiftUICustomPart01App.swift
+++ CoreData&SwiftUI01/CoreData&SwiftUIPart01/CoreData&SwiftUICustomPart01/CoreData&SwiftUICustomPart01/CoreData_SwiftUICustomPart01App.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct CoreData_SwiftUICustomPart01App: App {
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
