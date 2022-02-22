443975cb01656fce815308d7ab4db5af3adb609e
PetroOnishchuk <petroonishchuk@icloud.com>
2022-02-22 14:41:58 +0200

    Start Project

---
+++ CoreData&SwiftUI01/CoreData&SwiftUIPart03/CoreData&SwiftUIPart03StartPoint/CoreData&SwiftUIPart03/CoreData&SwiftUIPart03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreData&SwiftUIPart03
+//
+//  Created by Petro Onishchuk on 2/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreData&SwiftUI01/CoreData&SwiftUIPart03/CoreData&SwiftUIPart03StartPoint/CoreData&SwiftUIPart03/CoreData&SwiftUIPart03/CoreData_SwiftUIPart03App.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_SwiftUIPart03App.swift
+//  CoreData&SwiftUIPart03
+//
+//  Created by Petro Onishchuk on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_SwiftUIPart03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
