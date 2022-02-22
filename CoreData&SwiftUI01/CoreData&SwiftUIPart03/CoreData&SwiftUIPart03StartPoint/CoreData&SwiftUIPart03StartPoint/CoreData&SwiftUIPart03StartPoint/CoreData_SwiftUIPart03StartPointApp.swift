489782ecd0529c62f561e49714bed38012dc33b1
PetroOnishchuk <petroonishchuk@icloud.com>
2022-02-22 13:51:44 +0200

    Start Project CoreData&SwiftUIPart03StartPoint

---
+++ CoreData&SwiftUI01/CoreData&SwiftUIPart03/CoreData&SwiftUIPart03StartPoint/CoreData&SwiftUIPart03StartPoint/CoreData&SwiftUIPart03StartPoint/CoreData_SwiftUIPart03StartPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_SwiftUIPart03StartPointApp.swift
+//  CoreData&SwiftUIPart03StartPoint
+//
+//  Created by Petro Onishchuk on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_SwiftUIPart03StartPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
