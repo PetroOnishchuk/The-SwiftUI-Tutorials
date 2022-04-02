0c0a170c2e766354f2dbcc193509ef565f5052c4
PetroOnishchuk <petroonishchuk@icloud.com>
2022-04-02 19:28:28 +0300

    Start New Project HTTPServerAndGo01

---
+++ CoreData&SwiftUI01/CoreData&SwiftUIPart05/URLSession&PostRequestSwiftUI03FMPart01/URLSession&PostRequestSwiftUI03FMPart01/URLSession_PostRequestSwiftUI03FMPart01App.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_PostRequestSwiftUI03FMPart01App.swift
+//  URLSession&PostRequestSwiftUI03FMPart01
+//
+//  Created by Petro Onishchuk on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_PostRequestSwiftUI03FMPart01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
