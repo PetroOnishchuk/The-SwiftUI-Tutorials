c9e336714801dd7600e832786091d1d6af03e1dc
PetroOnishchuk <petroonishchuk@icloud.com>
2022-04-03 15:45:37 +0300

    project HTTPServerAndGo01 completed for GET Req

--- URLSessionAndGETRequest02/HTTPServerAndGo01/main.go
+++ URLSessionAndGETRequest02/HTTPServerAndGo01/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+
+	http.HandleFunc("/json", HandleJsonRequest)
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Printf("Error: %v", err.Error())
+	}
 
 }
--- URLSessionAndGETRequest02/HTTPServerAndGo01/user.go
+++ URLSessionAndGETRequest02/HTTPServerAndGo01/user.go
@@ -3,7 +3,7 @@ package main
 type User struct {
 	Id         int    `json:"id"`
 	Email      string `json:"email"`
-	First_name string `json:"firs_name"`
+	First_name string `json:"first_name"`
 	Last_name  string `json:"last_name"`
 	Avatar     string `json:"avatar"`
 }
