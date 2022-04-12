25eaae8abdba80f5d9b0d8a861f90e39d5cb99d8
PetroOnishchuk <petroonishchuk@icloud.com>
2022-04-12 18:24:30 +0300

    Start Project URLSessionAndGETRequestAsyncAwait01 and create model

---
+++ URLSessionAndSwiftUIasync:await/URLSessionAndGETRequestAsync:await01/URLSessionAndGETRequestAsyncAwait01/URLSessionAndGETRequestAsyncAwait01/Model/Page+UserModel.swift
@@ -0,0 +1,66 @@
+//
+//  Page+UserModel.swift
+//  URLSessionAndGETRequestAsyncAwait01
+//
+//  Created by Petro Onishchuk on 4/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct User: Codable, Identifiable {
+    let id: Int
+    let email: String
+    let firstName: String
+    let lastName: String
+    let avatar: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case email
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case avatar
+    }
+}
+
+struct Support: Codable {
+    let url: String
+    let text: String
+}
+
+
+struct AllUserData: Codable {
+    let user: User
+    let support: Support
+    
+    enum CodingKeys: String, CodingKey {
+        case user = "data"
+        case support
+    }
+}
+
+struct Page: Codable {
+    let page: Int
+    let perPage: Int
+    let total: Int
+    let totalPages: Int
+    var allUsers: [User]
+    let support: Support
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case perPage = "per_page"
+        case total
+        case totalPages = "total_Pages"
+        case allUsers = "data"
+        case support
+    }
+}
+
+enum MyRequestError: Error {
+    case invalidURL
+    case invalidData
+    case serverSideErrorWithResponse(Int)
+    case invalidHTTPURLResponse
+}
---
+++ URLSessionAndSwiftUIasync:await/URLSessionAndGETRequestAsync:await01/URLSessionAndGETRequestAsyncAwait01/URLSessionAndGETRequestAsyncAwait01/URLSessionAndGETRequestAsyncAwait01App.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionAndGETRequestAsyncAwait01App.swift
+//  URLSessionAndGETRequestAsyncAwait01
+//
+//  Created by Petro Onishchuk on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionAndGETRequestAsyncAwait01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
