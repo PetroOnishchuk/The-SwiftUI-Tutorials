8ff70111efa005e35ff5dc171983b21c92e3d2e8
PetroOnishchuk <petroonishchuk@icloud.com>
2022-06-09 18:17:37 +0300

    update main.go

--- URLSessionGenericDatabaseSwiftUIGolang01/GolangAndSQLite/main.go
+++ URLSessionGenericDatabaseSwiftUIGolang01/GolangAndSQLite/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	_ "modernc.org/sqlite"
 )
@@ -21,6 +22,30 @@ func main() {
 		fmt.Printf("DATABASE CLOSE: %v\n", d)
 	} else {
 		fmt.Printf("DATABASE Open\n")
+	}
+
+	// SELECT ALL USERS
+	http.HandleFunc("/users/", selectAllUsersRequest)
+
+	// SELECT COUPLE USERS
+	http.HandleFunc("/select", selectUsersPOSTRequest)
+
+	// SELECT SINGLE USER
+	http.HandleFunc("/selectSingle", selectSingleUserPOSTRequest)
+
+	// INSERT NEW USER
+	http.HandleFunc("/newUser", insertNewUserPOSTRequest)
+
+	// UPDATE USER
+	http.HandleFunc("/update", updateUserPOSTRequest)
+
+	// DELETE USER
+	http.HandleFunc("/delete", deleteUserPOSTRequest)
+
+	serverErr := http.ListenAndServe(":5000", nil)
+
+	if serverErr != nil {
+		fmt.Printf("SERVER Error: %v", serverErr)
 	}
 
 	defer DB.Close()
