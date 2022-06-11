8616247fec5176570c3b08d8823360a9191da1ed
PetroOnishchuk <petroonishchuk@icloud.com>
2022-06-11 13:36:06 +0300

    Start GolangAndMySQL project and setupm MySql

---
+++ URLSessionGenericDatabaseSwiftUIGolang01/GolangAndMySQL/go.mod
@@ -0,0 +1,5 @@
+module GolangAndMySQL
+
+go 1.18
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
---
+++ URLSessionGenericDatabaseSwiftUIGolang01/GolangAndMySQL/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Printf("MySQL Project")
+}
