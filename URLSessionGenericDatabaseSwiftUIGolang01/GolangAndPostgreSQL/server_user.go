27868989651f1fe177ec3395a4864ef73fbd8e85
PetroOnishchuk <petroonishchuk@icloud.com>
2022-06-16 18:36:25 +0300

    update and fill server_user

---
+++ URLSessionGenericDatabaseSwiftUIGolang01/GolangAndPostgreSQL/server_user.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+// SELECT ALL Users Rows
+func selectAllUsersRowsRequest(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "GET":
+		var allSelectedUsers = []User{}
+
+		reqFormValue := request.FormValue("users")
+		if reqFormValue == "" {
+			http.Error(writer, "wrong QueryItems", http.StatusBadRequest)
+		}
+		switch reqFormValue {
+		case "all":
+			writer.Header().Set("Content-Type", "application/json")
+			err := queryDatabaseAllUsers(&allSelectedUsers)
+			if err != nil {
+				http.Error(writer, err.Error(), http.StatusBadRequest)
+				return
+			}
+			newEncoder := json.NewEncoder(writer)
+			encodeErr := newEncoder.Encode(allSelectedUsers)
+
+			if encodeErr != nil {
+				http.Error(writer, encodeErr.Error(), http.StatusBadRequest)
+				return
+			}
+		default:
+			http.Error(writer, "wrong QueryValue", http.StatusBadRequest)
+			return
+		}
+	}
+}
+
+// SELECT Multiple Users Rows
+func selectMultipleUsersPOSTRequest(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "POST":
+		ctx := context.Background()
+
+		var allSelectedUsers = []User{}
+		writer.Header().Set("Content-Type", "application/json")
+
+		var searchUser User
+
+		newDecoder := json.NewDecoder(request.Body)
+		decodeErr := newDecoder.Decode(&searchUser)
+
+		if decodeErr != nil {
+			http.Error(writer, decodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+		selectErr := selectUsersRows(ctx, searchUser, &allSelectedUsers)
+
+		if selectErr != nil {
+			http.Error(writer, selectErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+		newEncoder := json.NewEncoder(writer)
+		encodeErr := newEncoder.Encode(allSelectedUsers)
+
+		if encodeErr != nil {
+			http.Error(writer, encodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+	}
+}
+
+// SELECT Single User
+func selectSingleUserPOSTRequest(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "POST":
+		writer.Header().Set("Content-Type", "application/json")
+
+		var searchUser User
+
+		newDecoder := json.NewDecoder(request.Body)
+		decodeErr := newDecoder.Decode(&searchUser)
+
+		if decodeErr != nil {
+			http.Error(writer, decodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+		selectedUser, err := selectSingleUsersRow(searchUser)
+
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		newEncoder := json.NewEncoder(writer)
+		encoderErr := newEncoder.Encode(selectedUser)
+
+		if encoderErr != nil {
+			http.Error(writer, encoderErr.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+}
+
+// INSERT New User
+func insertNewUserPOSTRequest(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "POST":
+		writer.Header().Set("Content-Type", "application/json")
+
+		var newUser User
+
+		newDecoder := json.NewDecoder(request.Body)
+		decodeErr := newDecoder.Decode(newUser)
+
+		if decodeErr != nil {
+			http.Error(writer, decodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+		id, err := insertNewUsersRow(newUser)
+
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+		newEncoder := json.NewEncoder(writer)
+		encodeErr := newEncoder.Encode(id)
+
+		if encodeErr != nil {
+			http.Error(writer, encodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+}
+
+// UPDATE User
+func updateUserPOSTRequest(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "POST":
+		writer.Header().Set("Content-Type", "application/json")
+
+		var updatingUser User
+
+		newDecoder := json.NewDecoder(request.Body)
+		decodeErr := newDecoder.Decode(updatingUser)
+
+		if decodeErr != nil {
+			http.Error(writer, decodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+		result, err := updateUsersRow(updatingUser)
+
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		newEncoder := json.NewEncoder(writer)
+		encodeErr := newEncoder.Encode(result)
+
+		if encodeErr != nil {
+			http.Error(writer, encodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+	}
+}
+
+// DELETE User
+func deleteUserPOSTRequest(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "POST":
+		writer.Header().Set("Content-Type", "application/json")
+
+		var deletingUser User
+
+		newDecoder := json.NewDecoder(request.Body)
+		decodeErr := newDecoder.Decode(deletingUser)
+
+		if decodeErr != nil {
+			http.Error(writer, decodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+
+		result, err := deleteUsersRow(deletingUser)
+
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		newEncoder := json.NewEncoder(writer)
+		encodeErr := newEncoder.Encode(result)
+
+		if encodeErr != nil {
+			http.Error(writer, encodeErr.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+}
