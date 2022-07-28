1a554f1b204638651610583ae05e91845254e714
PetroOnishchuk <petroonishchuk@icloud.com>
2022-07-28 14:06:18 +0300

    update Pool Project

---
+++ WebSocketsSwiftUIGolangWeb/GoWebSocketClientAndServer/GoWebSocketServerOneToManyPool01/ws_server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error with Conn: %v", err)
+		return nil, err
+	}
+	return conn, nil
+}
+
+func NewPool() *Pool {
+	newPool := &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+	return newPool
+}
