78c092b29961a24670d602f6289b796a808b195d
PetroOnishchuk <petroonishchuk@icloud.com>
2022-08-07 12:06:33 +0300

    Start New Project SwiftUIandAmazonSNSPush

---
+++ AmazonSNSandPushNotification01/SwiftUIandAmazonSNSPush/SwiftUIandAmazonSNSPush/SwiftUIandAmazonSNSPush/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIandAmazonSNSPush
+//
+//  Created by Petro Onishchuk on 8/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AmazonSNSandPushNotification01/SwiftUIandAmazonSNSPush/SwiftUIandAmazonSNSPush/SwiftUIandAmazonSNSPush/SwiftUIandAmazonSNSPushApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIandAmazonSNSPushApp.swift
+//  SwiftUIandAmazonSNSPush
+//
+//  Created by Petro Onishchuk on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIandAmazonSNSPushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
